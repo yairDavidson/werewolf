c8b1a9d3623fd01e76633f2e44bf9f22bb6c7fd3
yairDavidson <143421199+yairDavidson@users.noreply.github.com>
2025-07-16 22:19:37 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
-  projectId: "YOUR_FIREBASE_PROJECT_ID",
-  storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "YOUR_FIREBASE_APP_ID",
-};
+    const firebaseConfig = {
+  apiKey: "AIzaSyAE1dCOpq-60AOvsiA0Jjwp2fQzR6pT6Ho",
+  authDomain: "button-switch.firebaseapp.com",
+  databaseURL: "https://button-switch-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "button-switch",
+  storageBucket: "button-switch.firebasestorage.app",
+  messagingSenderId: "539954061254",
+  appId: "1:539954061254:web:fcffa655892c9094cfdcc7",
+  };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
